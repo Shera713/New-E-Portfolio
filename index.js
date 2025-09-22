98bcf3b6fff5dba2c0760eee1c338dc79ef18144
Shera713 <t.sheratoledo@gmail.com>
2025-09-22 10:28:42 -0400

    email

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
 //uRDEqdrCPH2hZgpNd
 
 function contact(event){
-event
-    console.log ('it worked')
+    event.preventDefault();
+    emailjs
+    .sendform(
+        'service_gmx8vf5',
+        'template_rcn8de8',
+        event.target,
+        'uRDEqdrCPH2hZgpNd'
+    ).then(() => {
+        console.log('this worked1')
+    })
 }
