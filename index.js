2ec402b5b835d165c98b73b6d2c56d3329df5003
Shera713 <t.sheratoledo@gmail.com>
2025-09-23 13:20:52 -0400

    shapes

--- index.js
+++ index.js
@@ -2,6 +2,33 @@
 //service_gmx8vf5
 //uRDEqdrCPH2hZgpNd
 
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = 'translate (${x * boolInt}px, ${y * boolInt}px)'
+    }
+}
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle
+    if(contrastToggle){
+        document.body.classList += " dark-theme"
+    }
+    else{
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
 function contact(event){
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
@@ -26,11 +53,10 @@ loading.classList += " modal__overlay--visable";
 
 }
 
-let isModalOpen = false;
 function toggleModal(){
     if(isModalOpen){
         isModalOpen = false;
-        return document.body.classList.remove("modal--open")
+        return document.body.classList.remove("modal--open");
     }
      isModalOpen = true;
     document.body.classList += " modal--open";
