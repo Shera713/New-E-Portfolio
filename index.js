fb6c5b6cacd355465029d63f7c2662e6613bccb8
Shera713 <t.sheratoledo@gmail.com>
2025-09-23 09:43:06 -0400

    toggle

--- index.js
+++ index.js
@@ -4,13 +4,34 @@
 
 function contact(event){
     event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');
+loading.classList += " modal__overlay--visable";
     emailjs
-    .sendform(
+    .sendForm(
         'service_gmx8vf5',
         'template_rcn8de8',
         event.target,
-        'uRDEqdrCPH2hZgpNd'
+       'uRDEqdrCPH2hZgpNd'
     ).then(() => {
-        console.log('this worked1')
+       loading.classList.remove("modal__overlay--visable");
+    success.classList += " modal__overlay--visable";
+    }).catch(() => {
+         loading.classList.remove("modal__overlay--visable");
+         alert(
+            "The email service is temporarily unavailable. Please contact me directly through my email t.sheratoledo@gmail.com"
+         );
+        
     })
+
+}
+
+let isModalOpen = false;
+function toggleModal(){
+    if(isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+     isModalOpen = true;
+    document.body.classList += " modal--open";
 }
